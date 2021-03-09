790f985b164f1f47b74a57b1fc7b3da30fecdb2f
monia <moniatec@gmail.com>
2021-03-09 17:21:33 -0500

    set up firebase

---
+++ shopping-cart/src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDrLhXta9qXNDKXMe5bifpB_3Oa_kKaT10",
+    authDomain: "shopping-cart-f28b5.firebaseapp.com",
+    projectId: "shopping-cart-f28b5",
+    storageBucket: "shopping-cart-f28b5.appspot.com",
+    messagingSenderId: "1047973457013",
+    appId: "1:1047973457013:web:b5c81718f4e9a0a73f9c3c",
+    measurementId: "G-E6ETMHFHLE"
+};
