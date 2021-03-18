c6f1c8a75a80b801677be5c04fb26dac2851bbd5
monia <moniatec@gmail.com>
2021-03-18 14:14:14 -0400

    more update and set up

--- shopping-cart/src/Header.js
+++ shopping-cart/src/Header.js
@@ -1,8 +1,26 @@
 import React from 'react'
+import './Header.css'
 
 function Header() {
     return (
-        <div>
+        <div className='header'>
+            <img className='header__logo' src='http://pngimg.com/uploads/amazon/amazon_PNG11.png' />
+            <div className='header__search'>
+                <input className='header__searchInput'
+                    type='text' />
+            </div>
+            <div className='header__nav'>
+                <div className='header__option' >
+                    <span className='header__optionLineOne' ></span>
+                </div>
+                <div className='header__option' >
+
+                </div>
+                <div className='header__option' >
+
+                </div>
+            </div>
+
 
         </div>
     )
