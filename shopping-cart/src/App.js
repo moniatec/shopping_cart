7c155b7fcfb986f65680442fcc7ec808e3a36f70
monia <moniatec@gmail.com>
2021-04-05 18:15:05 -0400

    add search bar inot the nav bar

--- shopping-cart/src/App.js
+++ shopping-cart/src/App.js
@@ -5,7 +5,7 @@ import Header from './Header'
 function App() {
   return (
     <div className="app">
-      <h1>Hello everyone</h1>
+
       <Header />
     </div>
   );
--- shopping-cart/src/Header.js
+++ shopping-cart/src/Header.js
@@ -1,13 +1,16 @@
 import React from 'react'
 import './Header.css'
+import SearchIcon from '@material-ui/icons/Search'
 
 function Header() {
     return (
+
         <div className='header'>
             <img className='header__logo' src='http://pngimg.com/uploads/amazon/amazon_PNG11.png' />
             <div className='header__search'>
                 <input className='header__searchInput'
                     type='text' />
+                <SearchIcon className='header__searchIcon' />
             </div>
             <div className='header__nav'>
                 <div className='header__option' >
